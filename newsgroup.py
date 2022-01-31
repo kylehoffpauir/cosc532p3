d279c89ea4991bcaa92313c31ac3d034cec29c0a
kyle hoffpauir <kyle@hoffpauir.com>
2022-01-31 15:08:00 -0500

    stem begin:

--- newsgroup.py
+++ newsgroup.py
@@ -1,3 +1,4 @@
+
 from sklearn.datasets import fetch_20newsgroups
 from sklearn.datasets import fetch_20newsgroups_vectorized
 from pprint import pprint
@@ -7,9 +8,24 @@ import matplotlib.pyplot as plt
 from sklearn.cluster import KMeans
 from sklearn.feature_extraction.text import TfidfVectorizer
 from wordcloud import WordCloud
+from sklearn.naive_bayes import MultinomialNB
+from sklearn import metrics
+import nltk
+from nltk.stem.porter import PorterStemmer
 
-newsgroups_train = fetch_20newsgroups(subset='train',remove=('headers', 'footers', 'quotes'),)
-newsgroups_test = fetch_20newsgroups(subset='test',remove=('headers', 'footers', 'quotes'),)
+
+# feature selection --> distance measure --> clustering
+#  --> data abstraction --> final evaluation
+# https://scikit-learn.org/0.19/datasets/twenty_newsgroups.html
+# https://towardsdatascience.com/a-friendly-introduction-to-text-clustering-fa996bcefd04
+# https://towardsdatascience.com/clustering-documents-with-python-97314ad6a78d
+# https://medium.com/analytics-vidhya/how-to-determine-the-optimal-k-for-k-means-708505d204eb
+# https://www.analyticsvidhya.com/blog/2016/11/an-introduction-to-clustering-and-different-methods-of-clustering/
+# https://www.analyticsvidhya.com/blog/2019/05/beginners-guide-hierarchical-clustering/
+
+
+newsgroups_train = fetch_20newsgroups(subset='train', remove=('headers', 'footers', 'quotes'))
+newsgroups_test = fetch_20newsgroups(subset='test', remove=('headers', 'footers', 'quotes'))
 
 
 pprint(list(newsgroups_train.target_names))
@@ -25,12 +41,29 @@ print(x.shape)
 print(vectorizer.get_feature_names_out())
 """
 
-vectorizer = TfidfVectorizer(stop_words={'english'})
+def tokenize(text):
+    tokens = nltk.word_tokenize(text)
+    stems = []
+    for item in tokens:
+        stems.append(PorterStemmer().stem(item))
+    return stems
+
+english_stemmer = nltk.stem.SnowballStemmer('english')
+vectorizer = TfidfVectorizer(tokenizer=tokenize, stop_words={'english'})
+
 x = vectorizer.fit_transform(newsgroups_train.data)
+
+# testing our fit on the test set to see how good we get our clusters
+clf = MultinomialNB(alpha=.01)
+vectors_test = vectorizer.transform(newsgroups_test.data)
+clf.fit(x, newsgroups_train.target)
+pred = clf.predict(vectors_test)
+
+print(metrics.f1_score(pred, newsgroups_test.target, average='macro'))
+
 #print(x)
+"""
 Sum_of_squared_distances = []
-
-
 K = range(2,10)
 for k in K:
     km = KMeans(n_clusters=k, max_iter=200, n_init=10)
@@ -41,9 +74,9 @@ plt.xlabel('k')
 plt.ylabel('Sum_of_squared_distances')
 plt.title('Elbow Method For Optimal k')
 plt.show()
+"""
 
-
-true_k = 7
+true_k = 20
 model = KMeans(n_clusters=true_k, init='k-means++', max_iter=200, n_init=10)
 model.fit(x)
 labels=model.labels_
@@ -51,16 +84,7 @@ forums=pd.DataFrame(list(zip(newsgroups_train.target_names,labels)),columns=['ti
 print(forums.sort_values(by=['cluster']))
 
 
-# feature selection --> distance measure --> clustering
-#  --> data abstraction --> final evaluation
-# https://scikit-learn.org/0.19/datasets/twenty_newsgroups.html
-# https://towardsdatascience.com/a-friendly-introduction-to-text-clustering-fa996bcefd04
-# https://towardsdatascience.com/clustering-documents-with-python-97314ad6a78d
-# https://medium.com/analytics-vidhya/how-to-determine-the-optimal-k-for-k-means-708505d204eb
-# https://www.analyticsvidhya.com/blog/2016/11/an-introduction-to-clustering-and-different-methods-of-clustering/
-# https://www.analyticsvidhya.com/blog/2019/05/beginners-guide-hierarchical-clustering/
-
-
+"""
 result={'cluster':labels,'forum':newsgroups_train.data}
 result=pd.DataFrame(result)
 for k in range(0,true_k):
@@ -78,6 +102,6 @@ for k in range(0,true_k):
     plt.axis("off")
     plt.show()
 
-
+"""
 
 
